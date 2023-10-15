f28a59b759963c06324d53aab7383d7c92680350
Mario Krutzikowsky <135688240+markruzo@users.noreply.github.com>
2023-10-15 16:15:43 +0200

    Update category_grid_item.dart

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -29,7 +29,7 @@ class CategoryGridItem extends StatelessWidget {
         child: Text(category.title,
             style: Theme.of(context).textTheme.titleMedium!.copyWith(
                   color: Theme.of(context).colorScheme.onBackground,
-                )),
+                ),),
       ),
     );
   }
