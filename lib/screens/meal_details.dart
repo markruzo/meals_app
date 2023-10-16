5e87866de08de91ca01f335ce3dfe789650235a5
Mario Krutzikowsky <mario.krutzikowsky@gmail.com>
2023-10-16 11:41:23 +0200

    HeroWidget

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -35,7 +35,7 @@ class MealDetailsScreen extends ConsumerWidget {
               );
             },
             icon: AnimatedSwitcher(
-              duration: const Duration(milliseconds: 400),
+              duration: const Duration(milliseconds: 300),
               transitionBuilder: (child, animation) {
                 return RotationTransition(
                   turns: Tween<double>(begin: 0.2, end: 1.0).animate(animation),
@@ -54,10 +54,13 @@ class MealDetailsScreen extends ConsumerWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.network(
-              meal.imageUrl,
-              width: double.infinity,
-              fit: BoxFit.cover,
+            Hero(
+              tag: meal.id,
+              child: Image.network(
+                meal.imageUrl,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
             ),
             const SizedBox(height: 14),
             Text(
